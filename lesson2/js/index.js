b49dd06353c593124fe678272eebdd744d6e8cc1
ailenrmansilla <man22007@byui.edu>
2022-04-29 17:56:30 -0300

    Js file added

---
+++ lesson2/js/index.js
@@ -0,0 +1,10 @@
+const LastModif = new Date(document.lastModified);
+const day = LastModif.getDate();
+const month = LastModif.getMonth();
+const year = LastModif.getFullYear();
+const hour = LastModif.getHours();
+const minute = LastModif.getMinutes();
+const second = LastModif.getSeconds();
+const last_modified = `${month}/${day}/${year} ${hour}:${minute}:${second}`;
+
+document.getElementById('updated').textContent = last_modified;
