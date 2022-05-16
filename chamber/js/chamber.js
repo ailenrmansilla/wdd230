2a3cf9d3f4aada6bf3ddd3a8161e6f73206ce224
ailenrmansilla <man22007@byui.edu>
2022-05-16 14:13:36 -0300

    editing navigation bar

--- chamber/js/chamber.js
+++ chamber/js/chamber.js
@@ -4,3 +4,7 @@ function toggleMenu(){
 }
 const x=document.getElementById("hamburger-button");
 x.onclick= toggleMenu;
+
+const today = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(today);
+document.getElementById("current-date").textContent=fulldate;
