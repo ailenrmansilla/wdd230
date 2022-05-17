c26e58d0091cafc54f8579f8ee31571c7743efee
ailenrmansilla <man22007@byui.edu>
2022-05-17 20:36:46 -0300

    CSS and Js fixed

--- chamber/js/chamber.js
+++ chamber/js/chamber.js
@@ -1,10 +1,28 @@
+const LastModif = new Date(document.lastModified);
+const day = LastModif.getDate();
+const month = LastModif.getMonth();
+const year = LastModif.getFullYear();
+const hour = LastModif.getHours();
+const minute = LastModif.getMinutes();
+const second = LastModif.getSeconds();
+const last_modified = `${month}/${day}/${year} ${hour}:${minute}:${second}`;
+
+document.querySelector('#updated').textContent = last_modified;
+
 function toggleMenu(){
+    console.log('Checking')
     document.getElementById("primary-nav").classList.toggle("open");
 
 }
 const x=document.getElementById("hamburger-button");
 x.onclick= toggleMenu;
 
+window.onresize = () => {if(window.innerWidth>992){
+    document.getElementById("primary-nav").classList.remove("open");
+}
+};
+
 const today = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(today);
+console.log(fulldate);
 document.getElementById("current-date").textContent=fulldate;
