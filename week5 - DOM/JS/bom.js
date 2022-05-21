c02d4953ee7d441e6a602f61d78d830104afd157
ailenrmansilla <man22007@byui.edu>
2022-05-21 19:18:36 -0300

    JS corrected but not working yet

--- week5 - DOM/JS/bom.js
+++ week5 - DOM/JS/bom.js
@@ -2,14 +2,26 @@ const inputChapter =document.querySelector('input');
 const button =document.querySelector('button');
 const list = document.querySelector('ul');
 
-button.addEventListener('click', function(inputChapter) {
+button.addEventListener('click', () => {
     const chapter = inputChapter.value;
-    if (chapter != ''){
-        const li_element = document.createElement('li');
-        li_element.textContent = chapter;
-        const delete_button = document.createElement('button');
-        delete_button.textContent ='❌';
+    inputChapter.value = '';
 
-    }
+    const li_element = document.createElement('li');
+    const listChapter = document.createElement('span')
+    li_element.appendChild(listChapter);
+    listChapter.textContent = chapter;
+    
+    const delete_button = document.createElement('button');
+    delete_button.textContent ='❌';
+    delete_button.ariaLabel('Remove chapter');
+
+    li_element.append(delete_button);
+    list.appendChild(li_element);
+
+    delete_button.addEventListener('click', () =>{
+        list.removeChild(li_element);
+    });
+    inputChapter.focus();
+
 
 });
