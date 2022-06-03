48bb2910d553619ead9ea949038c479e9e7428bb
ailenrmansilla <man22007@byui.edu>
2022-06-03 18:19:48 -0300

    Changes, splitted into weeks

---
+++ lesson-7/JS/lazyload.js
@@ -0,0 +1,42 @@
+let imagesToLoad = document.querySelectorAll('img[data-src]');
+
+// const loadImages = (image) => {
+//   image.setAttribute('src', image.getAttribute('data-src'));
+//   image.onload = () => {
+//     image.removeAttribute('data-src');
+//   };
+// };
+
+// const imageOptions = {};
+
+// imagesToLoad.forEach((img) => {
+//     loadImages(img);
+//   });
+
+function preloadImage(image) {
+    const src = image.getAttribute('data-src');
+    if (!src){
+        return;
+    } 
+    image.src = src; // set the real src of the image
+};
+
+const imageOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px 20px 0px"
+};
+
+const imageObserver = new IntersectionObserver((entries,imageObserver) =>{
+   // this is  the call back function
+    entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+        return; // if it is not intersection
+    } else{
+        preloadImage(entry.target);  // load it
+        imageObserver.unobserve(entry.target);
+    }
+})} , imageOptions);
+
+imagesToLoad.forEach(image =>{
+    imageObserver.observe(image);
+});
