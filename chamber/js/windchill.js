67454b8e9c6579404119ebce08a76e596936fd2f
ailenrmansilla <man22007@byui.edu>
2022-06-07 17:32:54 -0300

    optimizing images

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -3,4 +3,6 @@ const windspeed = parseFloat(document.getElementById('windspeed').textContent);
  if (temperature <= 50 && windspeed > 3){
     const windchill = 35.74 + (0.6215 * temperature) - 35.75 * (Math.pow(windspeed, 0.16)) + 0.4275 * temperature * (Math.pow(windspeed, 0.16))
     document.getElementById('windchill').textContent = Math.round(windchill);
+ } else{
+   document.getElementById('windchill').textContent = 'N/A';
  };
