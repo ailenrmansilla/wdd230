dc4dddc42603a457200503e975fb540320380646
ailenrmansilla <man22007@byui.edu>
2022-06-17 19:33:45 -0300

    Directory created

--- chamber/js/chamber.js
+++ chamber/js/chamber.js
@@ -86,3 +86,62 @@ numVisits++;
 localStorage.setItem("visits-ls", numVisits);
 visitsDisplay.textContent = numVisits;
 
+//json and fecth api code and buttons event listeners
+const url = "https://ailenrmansilla.github.io/wdd230/chamber/json/data.json"
+const contacts = document.querySelector('.directory-contacts');
+
+fetch(url)
+.then(function(response){
+    return response.json();
+})
+.then(function(jsonObject){
+    console.table(jsonObject);
+    const contacts = jsonObject['directory'];
+    contacts.forEach(displayDirectory);
+})
+
+function displayDirectory(contact){
+    let card = document.createElement('section');
+    let name = document.createElement('h2');
+    let picture = document.createElement('img');
+    let address = document.createElement('p');
+    let phone = document.createElement('p');
+    let website = document.createElement('p');
+    let membership_level = document.createElement('p');
+
+    picture.setAttribute('src', contact.icon);
+    let alt_text = `Icon of ${contact.name} - ${contact.membershiplevel} member of the Chamber`;
+    picture.setAttribute('alt', alt_text);
+    picture.setAttribute('loading','lazy');
+
+    name.textContent = contact.name;
+    address.textContent = contact.address;
+    phone.textContent = contact.phone;
+    website.textContent = contact.website;
+    membership_level = contact.membershiplevel;
+
+    card.appendChild(name);
+    card.appendChild(picture);
+    card.appendChild(phone);
+    card.appendChild(website);
+    card.appendChild(address);
+    card.appendChild(membership_level);
+    
+    document.querySelector('div.directory-contacts').appendChild(card);
+}
+// depending on what view they choose
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("div.directory-contacts");
+
+gridbutton.addEventListener("click", () => {
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList);
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+}
