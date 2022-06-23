d1cfbe19953a94151b13df7b65fb93b13b2c9050
ailenrmansilla <man22007@byui.edu>
2022-06-23 20:32:38 -0300

    error with JS solved

--- chamber/js/chamber.js
+++ chamber/js/chamber.js
@@ -31,9 +31,10 @@ console.log(fulldate);
 document.getElementById("current-date").textContent = fulldate;
 
 //for the join.html page
-const time = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
-document.querySelector("#time").value = time;
-document.querySelector('#date').value = fulldate.toString();
+
+// const time = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
+// document.querySelector("#time").value = time;
+// document.querySelector('#date').value = fulldate.toString();
 
 //banner on mondays and tuesdays
 const today_day = today.getDate();
@@ -75,76 +76,73 @@ imagesToLoad.forEach(image =>{
 
 // local storage
 
-// initialize display elements
-const visitsDisplay = document.querySelector("#visited-times");
-
 // get the stored value in localStorage
 let numVisits = Number(window.localStorage.getItem("visits-ls"));
-
-
 // increment the number of visits.
 numVisits++;
 // store the new number of visits value
 localStorage.setItem("visits-ls", numVisits);
-document.querySelector("#visited-times").textContent = numVisits;
+// initialize display elements
+const visitsDisplay = document.querySelector("#your-visits");
+visitsDisplay.textContent = `You visited this site ${numVisits} times`;
 
-//json and fecth api code and buttons event listeners
-const url = "https://ailenrmansilla.github.io/wdd230/chamber/json/data.json"
-const contactsDirectory = document.querySelector('.directory-contacts');
+// //json and fecth api code and buttons event listeners
+// const url = "https://ailenrmansilla.github.io/wdd230/chamber/json/data.json";
+// const contactsDirectory = document.querySelector('.directory-contacts');
 
-fetch(url)
-.then(function(response){
-    return response.json();
-})
-.then(function(jsonObject){
-    console.table(jsonObject);
-    const contacts = jsonObject['directory'];
-    contacts.forEach(displayDirectory);
-})
+// fetch(url)
+// .then(function(response){
+//     return response.json();
+// })
+// .then(function(jsonObject){
+//     console.table(jsonObject);
+//     const contacts = jsonObject['directory'];
+//     contacts.forEach(displayDirectory);
+// })
 
-function displayDirectory(contact){
-    console.log(contact);
-    let card = document.createElement('section');
-    let name = document.createElement('h2');
-    let picture = document.createElement('img');
-    let address = document.createElement('p');
-    let phone = document.createElement('p');
-    let website = document.createElement('p');
-    let membership_level = document.createElement('p');
+// function displayDirectory(contact){
+//     console.log(contact);
+//     let card = document.createElement('section');
+//     let name = document.createElement('h2');
+//     let picture = document.createElement('img');
+//     let address = document.createElement('p');
+//     let phone = document.createElement('p');
+//     let website = document.createElement('p');
+//     let membership_level = document.createElement('p');
 
-    picture.setAttribute('src', contact.icon);
-    let alt_text = `Icon of ${contact.name} - ${contact.membershiplevel} member of the Chamber`;
-    picture.setAttribute('alt', alt_text);
-    picture.setAttribute('loading','lazy');
+//     picture.setAttribute('src', contact.icon);
+//     let alt_text = `Icon of ${contact.name} - ${contact.membershiplevel} member of the Chamber`;
+//     picture.setAttribute('alt', alt_text);
+//     picture.setAttribute('loading','lazy');
 
-    name.textContent = contact.name;
-    address.textContent = contact.address;
-    phone.textContent = contact.phone;
-    website.textContent = contact.website;
-    membership_level.textContent = `${contact.membershiplevel} member`;
+//     name.textContent = contact.name;
+//     address.textContent = contact.address;
+//     phone.textContent = contact.phone;
+//     website.textContent = contact.website;
+//     membership_level.textContent = `${contact.membershiplevel} member`;
 
-    card.appendChild(name);
-    card.appendChild(picture);
-    card.appendChild(phone);
-    card.appendChild(website);
-    card.appendChild(address);
-    card.appendChild(membership_level);
+//     card.appendChild(name);
+//     card.appendChild(picture);
+//     card.appendChild(phone);
+//     card.appendChild(website);
+//     card.appendChild(address);
+//     card.appendChild(membership_level);
     
-    contactsDirectory.appendChild(card);
-}
-// depending on what view they choose
-const gridbutton = document.querySelector("#grid");
-const listbutton = document.querySelector("#list");
-const display = document.querySelector(".directory-contacts");
+//     contactsDirectory.appendChild(card);
+// }
+// // depending on what view they choose
+// const gridbutton = document.querySelector("#grid");
+// const listbutton = document.querySelector("#list");
+// const display = document.querySelector(".directory-contacts");
 
-gridbutton.addEventListener("click", () => {
-	display.classList.add("grid");
-	display.classList.remove("list");
-});
+// gridbutton.addEventListener("click", () => {
+// 	display.classList.add("grid");
+// 	display.classList.remove("list");
+// });
 
-listbutton.addEventListener("click", showList);
+// listbutton.addEventListener("click", showList);
 
-function showList() {
-	display.classList.add("list");
-	display.classList.remove("grid");
-}
+// function showList() {
+// 	display.classList.add("list");
+// 	display.classList.remove("grid");
+// }
